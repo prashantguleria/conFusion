b6edcf3b3f9d3863e538b597bb75bfea4701710d
prashantguleria <aguleria1@gmail.com>
2016-12-15 12:40:20 +0530

    Updated Angular Forms Exercise

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -67,4 +67,38 @@ var app = angular.module('confusionApp',[]).controller('MenuController',['$scope
         return($scope.tab === selectedTab);
 
     }
+}])
+
+.controller('ContactController',['$scope',function($scope){
+    
+
+    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                               agree:false, email:"" };
+    
+      var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+                        $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+    
+    
+    
+}])
+.controller('FeedbackController',['$scope',function($scope){
+    
+     $scope.sendFeedback = function() {
+                                console.log($scope.feedback);
+                                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+                                     $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                       agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+    
 }]);
