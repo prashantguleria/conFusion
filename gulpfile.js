9d47a2e455fbb7d491cb518fbed5f1a24d58289e
prashantguleria <aguleria1@gmail.com>
2016-12-26 11:06:07 +0530

    Json-server exercise done

--- gulpfile.js
+++ gulpfile.js
@@ -37,8 +37,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-    return gulp.src('./app/menu.html')
-        .pipe(usemin({
+return gulp.src('./app/**/*.html')        .pipe(usemin({
             css:[minifycss(),rev()],
             js: [ngannotate(),uglify(),rev()]
         }))
@@ -79,11 +78,11 @@ gulp.task('browser-sync', ['default'], function () {
     ];
 
     browserSync.init(files, {
-        server: {
-            baseDir: "dist",
-            index: "menu.html"
-        }
-    });
-    // Watch any files in dist/, reload on change
+      server: {
+         baseDir: "dist",
+         index: "index.html"
+      }
+   });
+   //  Watch any files in dist/, reload on change
     gulp.watch(['dist/**']).on('change', browserSync.reload);
 });
